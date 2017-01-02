87e2379e03392fd708fe7bd5cf2b47092e567cc4
FanYucai <fanyucai@126.com>
2017-01-02 10:34:28 +0800

    add who.c

--- linux-0.11/kernel/who.c
+++ linux-0.11/kernel/who.c
@@ -1,4 +1,41 @@
+
 #define __LIBRARY__
 #include <unistd.h>
-_syscall1(int, iam, const char*, name);
-_syscall2(int, whoami,char*,name,unsigned int,size);
+#include <errno.h>
+#include <asm/segment.h>
+
+
+char temp[64]={0};
+
+
+int sys_iam(const char* name)
+{
+   int i=0;
+   while(get_fs_byte(name+i)!='\0')
+        i++;
+   if(i>23){
+           return -EINVAL;
+      }
+   printk("%d\n",i);
+   i=0;
+   while((temp[i]=get_fs_byte(name+i))!='\0'){
+   i++;
+       }
+    return i;
+}
+
+
+int sys_whoami(char* name,unsigned int size)
+{
+    int i=0;
+    while (temp[i]!='\0')
+i++;
+    if (size<i)
+    return -1;
+    i=0;
+    while(temp[i]!='\0'){
+    put_fs_byte(temp[i],(name+i));
+    i++;
+        }
+    return i;
+}
