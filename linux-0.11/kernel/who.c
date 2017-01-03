f03c76455c386b696e5ed827a44a5a9d8dccb956
FanYucai <fanyucai@126.com>
2017-01-03 15:55:49 +0800

    ..

--- linux-0.11/kernel/who.c
+++ linux-0.11/kernel/who.c
@@ -2,51 +2,39 @@
 #include <errno.h>
 #include <asm/segment.h>
 
-char msg[24]; //23个字符 +'\0' = 24
+char msg[24]; //need including '\0'
 
 int sys_iam(const char * name)
-
 {
     //printk("im in iam");
-        int i;
-            char tmp[30];
-                for(i=0; i<30; i++)
-                {
-                            tmp[i] = get_fs_byte(name+i);
-                                    if(tmp[i] == '\0') break;  //字符串结束
+    int i;
+    char tmp[30];
+    for(i=0; i<30; i++)
+	{
+        tmp[i] = get_fs_byte(name+i);
+        if(tmp[i] == '\0') break;
+	}
+	i=0;
+    while(i<30&&tmp[i]!='\0') i++;
+    if(i > 23)
+        return -(EINVAL);
+    strcpy(tmp,msg);
 
-                }
-                    i=0;
-                        while(i<30&&tmp[i]!='\0') i++;
-                            int len = i;
-
-                                if(len > 23)
-                                {
-                                            return -(EINVAL);
-
-                                }
-                                    strcpy(msg,tmp);
-
-                                        return i;
-
+    return i;
 }
 
 int sys_whoami(char* name, unsigned int size)
 {
     //printk("im in whoami");
-    int len = 0;
-    for(;msg[len]!='\0';len++);
+    int len;
+    for(len = 0; msg[len]!='\0'; len++);
     if(len > size)
-    {
-                return -(EINVAL);
-
-    }
-    int i = 0;
+        return -(EINVAL);
+    int i;
     for(i=0; i<size; i++)
     {
-                put_fs_byte(msg[i],name+i);
-                        if(msg[i] == '\0') break; //字符串结束
-
+        put_fs_byte(msg[i],name+i);
+        if(msg[i] == '\0') break;
     }
     return i;
 }
